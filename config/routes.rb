ed9a3fe5e374a38ea8783ce8d737c69d0daf30b5
bam <peto@bam.sk>
2014-02-27 12:59:11 +0100

    static page contact

--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,8 @@ Altcoinguard::Application.routes.draw do
     end
   end
 
+  get "pages/show"
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
